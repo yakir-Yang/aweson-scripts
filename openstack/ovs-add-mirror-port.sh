e4d15f60675d584b1b617ca6dc3267d00ad749ff
Kuankuan Yang <kkyang@astri.org>
2017-12-20 14:45:21 +0800

    openstack/ovs-add-mirror-port: add multiple dummy devices support
    Signed-off-by: Kuankuan Yang <kkyang@astri.org>

--- openstack/ovs-add-mirror-port.sh
+++ openstack/ovs-add-mirror-port.sh
@@ -2,35 +2,48 @@
 
 BRIDGE=${1:-br-int}
 PORT=${2:-"all"}
+DUMMY=${3:-dummy0}
+
+if [ ! -z $4 ]; then
+    ovs-vsctl clear bridge $BRIDGE mirrors
+    #ovs-vsctl --columns=_uuid list port $DUMMY | awk '{print $3}' |\
+    #    xargs -i ovs-vsctl remove mirror mirror-$DUMMY output_port={}
+    exit
+fi
 
 ovs-vsctl show|grep Bridge | awk '{print $2}' | sed "s/\"//g" | \
     while read br; do \
         ovs-vsctl clear bridge $br mirrors > /dev/null; \
-        ovs-vsctl del-port $br dummy0 2> /dev/null; \
+        ovs-vsctl del-port $br $DUMMY 2> /dev/null; \
     done;
 
-# Set up dummy0 device
+# Set up $DUMMY device
 modprobe dummy
-ip link set up dummy0
 
-# Add 'dummy0' as the mirror port of bridge
-ovs-vsctl add-port $BRIDGE dummy0
+ifconfig $DUMMY 2>&1 > /dev/null
+if [ $? -ne 0 ]; then
+    ip link add $DUMMY type dummy
+fi
+ip link set up $DUMMY
 
-ovs-vsctl --id=@m create mirror name=mirror-$BRIDGE -- add bridge $BRIDGE mirrors @m
+# Add '$DUMMY' as the mirror port of bridge
+ovs-vsctl add-port $BRIDGE $DUMMY
 
-ovs-vsctl --columns=_uuid list port dummy0 | awk '{print $3}' |\
-    xargs -i ovs-vsctl set mirror mirror-$BRIDGE output_port={}
+ovs-vsctl --id=@m create mirror name=mirror-$DUMMY -- add bridge $BRIDGE mirrors @m
+
+ovs-vsctl --columns=_uuid list port $DUMMY | awk '{print $3}' |\
+    xargs -i ovs-vsctl set mirror mirror-$DUMMY output_port={}
 
 # Listen all traffic input/output from bridge
 if [ "$PORT" == "all" ]; then
     echo -e "\033[32m$BRIDGE: list all ports\033[0m"
-    ovs-vsctl set mirror mirror-$BRIDGE select_all=1
+    ovs-vsctl set mirror mirror-$DUMMY select_all=1
 else
     echo -e "\033[32m$BRIDGE: list port '$PORT'\033[0m"
     PORT_UUID=`ovs-vsctl --columns=_uuid list port $PORT | awk '{print $3}'`
-    ovs-vsctl set mirror mirror-$BRIDGE select\_dst\_port=$PORT_UUID
-    ovs-vsctl set mirror mirror-$BRIDGE select\_src\_port=$PORT_UUID
+    ovs-vsctl set mirror mirror-$DUMMY select\_dst\_port=$PORT_UUID
+    ovs-vsctl set mirror mirror-$DUMMY select\_src\_port=$PORT_UUID
 fi
 
 # List the exist mirror port for checking
-ovs-vsctl list mirror
+ovs-vsctl list mirror mirror-$DUMMY
