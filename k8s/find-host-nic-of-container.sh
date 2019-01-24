4f645eb6a976955e471860029f47a98b33d668dc
Kuankuan Yang <kkyang@astri.org>
2019-01-24 11:45:28 +0800

    k8s: find the peer host veth pair of container network interface
    $ sudo ./find-host-nic-of-container.sh  sdwan-log-logstash
    ContainerName:	 k8s_sdwan-log-logstash_sdwan-log-logstash-2qn58_default_5ffdf8ae-1d28-11e9-ba8d-000c29a61911_0
    ContainerNic:	 eth0
    HostNic:	 veth4a3607c2
    
    Signed-off-by: Kuankuan Yang <kkyang@astri.org>

---
+++ k8s/find-host-nic-of-container.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "$0 <container_name> [container_nic]"
+    exit
+fi
+
+ContainerName=$1
+ContainerNic=$2
+
+container=`docker ps | grep ${ContainerName} | grep -v pause | awk '{print $NF}'`
+if [ -z $container ]; then
+    echo "Invalid container name: ${ContainerName}"
+    exit
+fi
+
+if [ ! -z $ContainerNic ]; then
+    nic=$ContainerNic
+else
+    for n in `docker exec -it ${container} bash -c "ls /sys/class/net"`; do
+        n=`echo $n | tr -d '\r'`
+        if [ $n != "lo" ]; then
+            nic="$nic $n"
+        fi
+    done
+fi
+
+for n in $nic; do
+    # find the container nic peer ifindex
+    iflink=`docker exec -it ${container} bash -c "cat /sys/class/net/${n}/iflink"`
+    iflink=`echo $iflink|tr -d '\r'`
+
+    veth=`grep -l $iflink /sys/class/net/veth*/ifindex`
+    veth=`echo $veth | sed -e 's;^.*net/\(.*\)/ifindex$;\1;'`
+
+    echo -e "ContainerName:\t $container"
+    echo -e "ContainerNic:\t $n"
+    echo -e "HostNic:\t $veth"
+done
